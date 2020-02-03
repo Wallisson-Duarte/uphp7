ee59ddcdbb11ed2fa358a03ac698ab3c196bba29
Wallisson Duarte <w.eduardo.wow.7@gmail.com>
2020-02-03 18:37:41 -0300

    adicionando o conteudo sobre variaveis

---
+++ variaveis/01-conceito.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // No PHP não há tipagem de dados, ou seja, não precisa definir qual o tipo dado
+            $name = "Wallisson";
+            echo "nome: $name<br/>";
+        // Mostra o tipo, tamanho, e o conteudo de uma variavel.    
+            var_dump($name);
+        ?>
+    </body>
+</html>
